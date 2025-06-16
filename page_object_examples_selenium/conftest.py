e68b6ec3ea44c5d517a96538ca16db57d9d819d7
biruzka <biruzka@gmail.com>
2025-06-16 13:15:46 +0300

    option detach added to driver

--- page_object_examples_selenium/conftest.py
+++ page_object_examples_selenium/conftest.py
@@ -22,6 +22,7 @@ def initialize_driver(headless=False):
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         options.add_argument("--window-size=1920,1080")
+        options.add_experimental_option("detach", True)
 
     service = Service(executable_path="C:\\Projects\\python_projects\\python_tests_examples\\python_tests_examples\\chromedriver.exe")
     driver = webdriver.Chrome(service=service, options=options)
